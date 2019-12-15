71e7b4bc11914091faf8862575834de8e387414e
jiangjk2000 <3385706034@qq.com>
2019-12-15 17:03:32 +0800

    version1.0.0

---
+++ 数据采集服务/About.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace 数据采集服务
+{
+    partial class About
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Location = new System.Drawing.Point(18, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 241);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务器\r\n\r\n版权所有：JJK、LC工作室\r\n\r\n欢迎建议、咨询及解决方案定制！\r\n\r\nemail: 1411279054@qq.com\r\n\r\nQQ:141127" +
+    "9054\r\n\r\n如有相关建议,请发邮箱：\r\n\r\n";
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(38, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1411279054@qq.com";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "好的";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // About
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 341);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "About";
+            this.Text = "About";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ 数据采集服务/Settings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CCWin;
+
+namespace 数据采集服务
+{
+    public delegate void setTextValue(string textValue);
+
+    public partial class Settings : Skin_Mac
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+        public event setTextValue SetFormTextValue;
+
+        private void settingYes_Click(object sender, EventArgs e)
+        {
+            string value = settingTextBox1.Text + '#' + settingTextBox2.Text + '#' +textBox1.Text;
+            SetFormTextValue(value);
+            this.Close();
+        }
+
+        private void settingCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void viewFoldButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            if(dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.SelectedPath;
+            }
+        }
+
+
+    }
+}
